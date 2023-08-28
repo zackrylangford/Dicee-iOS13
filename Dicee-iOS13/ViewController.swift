34a1ae4acbfe7a9ee4ce32d463ff599870d156cb
Zackry Langford <zackrylangford@gmail.com>
2023-08-27 21:50:54 -0400

    Updated the dice and created the basic layout

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
     }
 
 
